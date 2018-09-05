2e6871b2c0b2d871c916214dba1f93939c4417c0
Alejandro <alejandro@biklabs-imac.dyn.ucr.edu>
2018-09-05 15:09:02 -0700

    Seq Count Summary and Quality

---
+++ 02-QIIME2_VisualizeSeq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name="VisualizeSeq"
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=7-00:00:00
+#SBATCH --mail-user=adesa002@ucr.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -e VisualizeSeq.err-%N
+#SBATCH -o VisualizeSeq.out-%N
+
+#Path Variables
+INPUT=/rhome/adesa002/shared/NPRB/18S/analysis-results/QIIME2018v8_18s_05Sep2018/NPRB_DemulSeq.qza
+OUTPUT=/rhome/adesa002/shared/NPRB/18S/analysis-results/QIIME2018v8_18s_05Sep2018/NPRB_VisDemulSeq.qzv
+
+#Activate QIIME2v2018.8
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
+source activate qiime2-2018.8
+
+#Script
+qiime demux summarize \
+--i-data $INPUT \
+--o-visualization $OUTPUT
